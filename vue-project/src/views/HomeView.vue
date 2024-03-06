7dccfbcee2a1dbfadaf5fab6fa057f4228548ffc
devinw50 <addicteddmg@gmail.com>
2024-03-06 09:57:02 -0500

    rth
    thr

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,23 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+ import { ref, onMounted } from "vue";
+ const apidata = ref("")
+ async function getApiData() {
+  let res = await fetch ("https://data.cityofnewyork.us/resource/7z8d-msnt.json")
+  let data = await res.json();
+  apidata.value = data;
+ }
+
+ onMounted (() => {
+  getApiData();
+ });
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
