d689c17621456addb57fea0b82669192aef82293
devinw50 <addicteddmg@gmail.com>
2024-03-12 10:05:03 -0400

    fw
    efw

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,23 +1,37 @@
 <template>
-  <div>
-
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
   </div>
 </template>
 
 <script>
- import { ref, onMounted } from "vue";
- const apidata = ref("")
- async function getApiData() {
-  let res = await fetch ("https://www.bklynlibrary.org/locations/json")
-  let data = await res.json();
-  apidata.value = data;
- }
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
- onMounted (() => {
-  getApiData();
- });
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async OnMounted () {
+    this.loaded = false
+
+    try {
+      const { userlist } = await fetch('https://www.bklynlibrary.org/locations/json')
+      this.chartdata = userlist
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
 </script>
 
 <style scoped>
-
 </style>
+
