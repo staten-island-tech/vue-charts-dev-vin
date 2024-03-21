c532eac7d3d145748112a14483a8b999f6787ce0
devinw50 <addicteddmg@gmail.com>
2024-03-21 09:47:05 -0400

    tyu
    tyu

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,37 +1,26 @@
-<template>
-  <div class="container">
-    <Bar v-if="loaded" :data="chartData" />
-  </div>
-</template>
+<script setup>
+import { ref,  } from 'vue';
+import {onBeforeMount} from 'vue';
 
-<script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+const nyc = ref('')
+async function api () {
+  try {
+  let fetchthing = await fetch ("https://data.cityofnewyork.us/resource/f9bf-2cp4.json")
+  let putasjson = await fetchthing.json();
+  nyc.value = putasjson;
+  console.log(putasjson)
+}  
+catch (error) {
+  console.log ("error", error)
+}}
+onBeforeMount(() =>{
+    api();
+})
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data: () => ({
-    loaded: false,
-    chartData: null
-  }),
-  async OnMounted () {
-    this.loaded = false
-
-    try {
-      const { userlist } = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
-      this.chartdata = userlist
-
-      this.loaded = true
-    } catch (e) {
-      console.error(e)
-    }
-  }
-}
 </script>
 
-<style scoped>
-</style>
 
+<template>
+  <main>
+  </main>
+</template>
