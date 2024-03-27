f581d71b0280d9d59d7bedd2dabd183f4871c2b9
devinw50 <addicteddmg@gmail.com>
2024-03-27 09:38:58 -0400

    Update HomeView.vue

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { ref,  } from 'vue';
 import {onBeforeMount} from 'vue';
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 const nyc = ref('')
 async function api () {
@@ -14,7 +18,7 @@ catch (error) {
   console.log ("error", error)
 }}
 onBeforeMount(() =>{
-    api();
+    api("https://data.cityofnewyork.us/resource/f9bf-2cp4.json");
 })
 
 </script>
