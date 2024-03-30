a9d7da85e111fd265745a96f57f696909280fe3a
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2024-03-29 20:14:16 -0600

    sda

---
+++ vue-project/src/components/PieChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="pie-chart">
+    <Pie :data="chartData" />
+  </div>
+</template>
+
+<script>
+import { Pie } from 'vue-chartjs'
+
+export default {
+  name: 'PieChart',
+  extends: Pie,
+  props: ['chartData'],
+  mounted() {
+    this.renderChart(this.chartData, { responsive: true, maintainAspectRatio: false })
+  }
+}
+</script>
+
+<style scoped>
+.pie-chart {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,32 +1,63 @@
-<script setup>
-import { ref,  } from 'vue';
-import {onBeforeMount} from 'vue';
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+<template>
+  <div class="home-view">
+    <PieChart :chartData="chartData" v-if="loaded" />
+    <div v-else>Loading...</div>
+  </div>
+</template>
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+<script>
+import PieChart from '@/components/PieChart.vue'
 
-const nyc = ref('')
-async function api () {
-  try {
-  let fetchthing = await fetch ("https://data.cityofnewyork.us/resource/f9bf-2cp4.json")
-  let putasjson = await fetchthing.json();
-  nyc.value = putasjson;
-  console.log(putasjson)
-}  
-catch (error) {
-  console.log ("error", error)
-}}
-onBeforeMount(() =>{
-    api("https://data.cityofnewyork.us/resource/f9bf-2cp4.json");
-})
+export default {
+  name: 'HomeView',
+  components: {
+    PieChart
+  },
+  data() {
+    return {
+      loaded: false,
+      chartData: null
+    }
+  },
+  async mounted() {
+    try {
+      const response = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      const data = await response.json()
 
+      const groupedData = {}
+      data.forEach(item => {
+        const ethnicity = item.race_ethnicity
+        const score = parseInt(item.sat_critical_reading_avg_score) + parseInt(item.sat_math_avg_score) + parseInt(item.sat_writing_avg_score)
+        if (!groupedData[ethnicity]) {
+          groupedData[ethnicity] = 0
+        }
+        groupedData[ethnicity] += score
+      })
 
+      const labels = Object.keys(groupedData)
+      const datasets = [{
+        data: Object.values(groupedData),
+        backgroundColor: labels.map(_ => '#' + Math.floor(Math.random()*16777215).toString(16)),
+        label: 'SAT Scores'
+      }]
 
+      this.chartData = { labels, datasets }
+      this.loaded = true
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+}
 </script>
 
-
-<template>
-  <main>
-  </main>
-</template>
+<style scoped>
+.home-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
